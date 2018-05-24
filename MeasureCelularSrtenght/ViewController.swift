419695402e04b26fd50860a874205566cba7a10f
Victor Cotrim <cotrim149@gmail.com>
2018-05-24 09:18:23 -0300

    Adding files to measure celular strenght

--- MeasureCelularSrtenght/ViewController.swift
+++ MeasureCelularSrtenght/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var celularStrenghtLabel: UILabel!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		let sinalValue = getSignalStrength()
+		celularStrenghtLabel.text = String(sinalValue)
 	}
-
 
 }
 
+extension ViewController {
+	
+	func getSignalStrength() -> Int {
+		
+		let application = UIApplication.shared
+		let statusBarView = application.value(forKey: "statusBar") as! UIView
+		
+		guard let foregroundView = statusBarView.value(forKey: "foregroundView") as? UIView else {
+			return 0
+		}
+		
+		let foregroundViewSubviews = foregroundView.subviews
+		
+		var dataNetworkItemView:UIView!
+		
+		for subview in foregroundViewSubviews {
+			if subview.isKind(of: NSClassFromString("UIStatusBarSignalStrengthItemView")!) {
+				dataNetworkItemView = subview
+				break
+			} else {
+				return 0 //NO SERVICE
+			}
+		}
+		
+		return dataNetworkItemView.value(forKey: "signalStrengthBars") as! Int
+		
+	}
+}
